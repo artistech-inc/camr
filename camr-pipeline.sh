6756f83e059b0b21795a8bbbaa482c242e87f4c3
Matthew Aguirre <matt.aguirre@gmail.com>
2017-02-03 10:46:06 -0500

    Added printing of the current step.

--- camr-pipeline.sh
+++ camr-pipeline.sh
@@ -2,7 +2,14 @@
 
 MODEL=amr-anno-1.0/amr-anno-1.0.train.basic-abt-brown-verb.m
 
+echo "python amr_parsing.py -m preprocess $1"
 python amr_parsing.py -m preprocess $1
+
+echo "python amr_parsing.py -m parse --model $MODEL $1"
 python amr_parsing.py -m parse --model $MODEL $1
+
+echo "python amr_parsing.py -m preprocess --amrfmt amr $1.all.basic-abt-brown-verb.parsed"
 python amr_parsing.py -m preprocess --amrfmt amr $1.all.basic-abt-brown-verb.parsed
+
+echo ""./scripts/jamr_align.sh $1.all.basic-abt-brown-verb.parsed.amr.tok"
 ./scripts/jamr_align.sh $1.all.basic-abt-brown-verb.parsed.amr.tok
--- tok-pipeline.sh
+++ tok-pipeline.sh
@@ -2,7 +2,5 @@
 
 MODEL=amr-anno-1.0/amr-anno-1.0.train.basic-abt-brown-verb.m
 
+echo "python amr_parsing.py -m preprocess $1"
 python amr_parsing.py -m preprocess $1
-#python amr_parsing.py -m parse --model $MODEL $1
-#python amr_parsing.py -m preprocess --amrfmt amr $1.all.basic-abt-brown-verb.parsed
-#./scripts/jamr_align.sh $1.all.basic-abt-brown-verb.parsed.amr.tok
